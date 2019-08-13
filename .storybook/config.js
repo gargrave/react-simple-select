4c722dd74cc70732152def61af21ed69cc09b1f2
Gabe Hargrave <gkhargrave@gmail.com>
2019-08-12 17:52:20 -0700

    Storybook config cleanup

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react'
 
-// automatically import all files ending in *.stories.js
+// automatically import all files ending in *.stories.tsx
 const req = require.context('../src', true, /\.stories\.tsx$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
