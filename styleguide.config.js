d4b0cf5e7c7f466a27237ca9cb5286a4af47bf7d
Gabe Hargrave <gkhargrave@gmail.com>
2019-11-23 20:06:30 -0800

    Add docs (Styleguidist build) dir for GitHub Pages

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 
+const pkg = require('./package.json')
+
 module.exports = {
   components: ['src/components/Select/Select.tsx'],
   propsParser: require('react-docgen-typescript').withCustomConfig(
@@ -10,6 +12,7 @@ module.exports = {
     path.join(__dirname, 'src/components/Select/styles/Select.scss'),
     path.join(__dirname, 'src/components/Select/styleguide/Styleguide.scss'),
   ],
-  title: 'React Simple Select',
+  styleguideDir: 'docs',
+  title: `React Simple Select ${pkg.version}`,
   webpackConfig: require('./webpack/webpack.config.js'),
 }
